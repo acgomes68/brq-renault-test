7e583bac9f2529ff0e020d1eb01af78c902768d7
acgomes68 <acgomes68@gmail.com>
2020-08-31 02:56:59 -0300

    Refatoração das rotas e da contoller de usuários

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,8 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import Url from '../models/Url';
+
+const validUrl = require('valid-url');
 
 class UserController {
     async index(req, res) {
@@ -41,11 +44,11 @@ class UserController {
                 return res.status(400).json({ error: 'User already exists' });
             }
 
-            const { id, name } = await User.create(req.body);
+            const { id, urls } = await Url.create(req.body);
 
             return res.json({
                 id,
-                name,
+                urls,
             });
         } catch (error) {
             return res.status(502).json({ error });
@@ -53,14 +56,44 @@ class UserController {
     }
 
     async update(req, res) {
-        const { id } = req.params;
+        const { user_id, urls } = req.body;
+        const schema = Yup.object().shape({
+            user_id: Yup.int().required(),
+            urls: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
         try {
-            const user = await User.findByPk(id);
+            const user = await User.findByPk(user_id);
             if (!user) {
                 return res.status(400).json({ error: 'User not found' });
             }
-            await user.destroy();
-            return res.json(user);
+
+            if (!validUrl.isUri(urls)) {
+                return res.status(400).json({ error: 'Invalid URL' });
+            }
+            // let hash = await models.storeURL(req.body.url);
+            // res.send(req.hostname + '/' +hash);
+            // catch(e) {
+            //     console.log(e);
+            //     res.send('error occurred while storing URL.');
+            // }
+            const hasItem = await Url.findOne({
+                where: { user_id, urls },
+            });
+
+            if (hasItem) {
+                return res.status(400).json({ error: 'URLS already exists' });
+            }
+
+            const { user_id, urls } = await Url.update(req.body);
+
+            return res.json({
+                user_id,
+                urls,
+            });
         } catch (error) {
             return res.status(502).json({ error });
         }
