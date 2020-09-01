9208e44692c58e8be929b3814fb60accacef8a58
acgomes68 <acgomes68@gmail.com>
2020-09-01 16:00:49 -0300

    Correção do incremento do hti

--- src/app/controllers/UrlController.js
+++ src/app/controllers/UrlController.js
@@ -7,16 +7,19 @@ class UrlController {
             const { id } = req.params;
 
             const urls = await Url.findByPk(id);
-            const { hits } = urls;
+            const { curHits } = urls.hits;
 
             if (!urls) {
                 return res.status(404).json({ error: 'URL not found' });
             }
 
-            urls.hits = hits + 1;
+            const newHits = curHits + 1;
+
+            const newUrls = await Url.findByIdAndUpdate(id, { hits: newHits });
+
             res.redirect(301, urls.url);
 
-            return true;
+            return res.json({ newUrls });
         } catch (error) {
             return res.status(502).json({ error });
         }
